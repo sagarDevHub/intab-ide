76db24421effcce380cb91a27f3b63f6bd17af60
sagarDevHub <gullasagar9933@gmail.com>
2026-06-19 18:31:12 +0530

    feat: initial setup with Next.js and Shadcn

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <Button>click me</Button>
+      <h1 className="text-red-400">hello</h1>
+    </div>
+  );
+};
+
+export default Home;
