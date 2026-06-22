eef0c83bc4c73108f2651fe6702d7ed4ef79f9ed
sagarDevHub <gullasagar9933@gmail.com>
2026-06-22 18:12:18 +0530

    refactor: Redis caching for authentication.

--- features/auth/server.ts
+++ features/auth/server.ts
@@ -1,11 +1,28 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from '@better-auth/prisma-adapter';
 import { db } from '@/lib/db';
+import { redis } from '@/lib/redis/redis';
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: 'mongodb',
   }),
+  secondaryStorage: {
+    get: async key => {
+      const value = await redis.get<string>(key);
+      return value ? JSON.stringify(value) : null;
+    },
+    set: async (key, value, ttl) => {
+      if (ttl) {
+        await redis.set(key, value, { ex: ttl });
+      } else {
+        await redis.set(key, value);
+      }
+    },
+    delete: async key => {
+      await redis.del(key);
+    },
+  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
